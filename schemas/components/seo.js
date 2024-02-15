5078d603154edf26d011dfacce9551aaf921369d
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-02-15 16:25:10 +0100

    added proper validation to SEO

--- schemas/components/seo.js
+++ schemas/components/seo.js
@@ -2,13 +2,17 @@ export default {
   name: "seo",
   title: "SEO",
   type: "object",
+  validation: Rule => Rule.required(),
   fields: [
     {
       name: 'title',
       type: 'string',
       title: 'Tytuł',
       description: 'Tytuł strony, który jest ważny ze względów SEO. Pojawia się np. w pasku przeglądarki oraz w wyszukiwarkach internetowych.',
-      validation: Rule => Rule.max(70).warning('Pole nie powinno być dłuższe niż 70 znaków.')
+      validation: Rule => [
+        Rule.max(70).warning('Pole nie powinno być dłuższe niż 70 znaków.'),
+        Rule.required(),
+      ]
     },
     {
       name: 'description',
@@ -16,7 +20,10 @@ export default {
       title: 'Opis',
       rows: 4,
       description: 'Opis strony, który jest ważny ze względów SEO. Pojawia się np. w wyszukiwarkach internetowych oraz podczas udostępniania strony na mediach społecznościowych.',
-      validation: Rule => Rule.max(165).warning('Pole nie powinno być dłuższe niż 165 znaków.')
+      validation: Rule => [
+        Rule.max(165).warning('Pole nie powinno być dłuższe niż 165 znaków.'),
+        Rule.required(),
+      ]
     },
   ]
 }
