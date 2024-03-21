1e5884f13483366204f74cea5ab50dbbff0c4ba2
Shembou <sshembou@gmail.com>
2024-03-21 15:57:41 +0100

    added HighlightedImage

--- schemas/collectionTypes/BlogPost_Collection.js
+++ schemas/collectionTypes/BlogPost_Collection.js
@@ -65,6 +65,11 @@ export default {
           return true;
         }).required(),
     },
+    {
+      name: 'content',
+      type: 'PortableText',
+      title: 'Zawartość bloga',
+    },
   ],
   fieldsets: [
     {
