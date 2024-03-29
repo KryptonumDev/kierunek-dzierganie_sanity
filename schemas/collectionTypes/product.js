91f0b907f96a69388f81dd71233c0dec0b719084
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-03-29 11:00:23 +0100

    added sections for description in product schema

--- schemas/collectionTypes/product.js
+++ schemas/collectionTypes/product.js
@@ -1,3 +1,205 @@
+import { countItems } from "../../utils/count-items";
+import { removeMarkdown } from "../../utils/functions";
+
+
+const ColumnImageSection = {
+  name: 'ColumnImageSection',
+  title: 'Sekcja z obrazkiem w kolumnie',
+  type: 'object',
+  fields: [
+    {
+      name: 'isReversed',
+      type: 'boolean',
+      title: 'Czy sekcja ma być odwrócona?',
+      description: 'Jeśli zaznaczone zdjęcie będzie po prawej stronie, a tekst po lewej.',
+      initialValue: false,
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: 'heading',
+      type: 'markdown',
+      title: 'Nagłówek (opcjonalny)',
+    },
+    {
+      name: 'suhheading',
+      type: 'markdown',
+      title: 'Podnagłówek (opcjonalny)',
+    },
+    {
+      name: 'paragraph',
+      type: 'markdown',
+      title: 'Paragraf',
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: 'img',
+      type: 'image',
+      title: 'Zdjęcie',
+      validation: Rule => Rule.required(),
+    },
+  ],
+  preview: {
+    select: {
+      heading: 'heading',
+      suhheading: 'suhheading',
+      paragraph: 'paragraph',
+      media: 'img',
+    },
+    prepare({ heading, suhheading, paragraph, media }) {
+      return {
+        title: removeMarkdown(heading) || removeMarkdown(suhheading),
+        subtitle: removeMarkdown(paragraph),
+        media,
+      };
+    }
+  }
+}
+const OrderedList = {
+  name: 'OrderedList',
+  title: 'Sekcja z listą numerowaną',
+  type: 'object',
+  fields: [
+    {
+      name: 'heading',
+      type: 'markdown',
+      title: 'Nagłówek',
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: 'list',
+      type: 'array',
+      of: [
+        { type: 'markdown' }
+      ],
+      title: 'Lista',
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: 'paragraph',
+      type: 'markdown',
+      title: 'Paragraf (opcjonalnie)',
+    },
+  ],
+  preview: {
+    select: {
+      heading: 'heading',
+      list: 'list',
+    },
+    prepare({ heading, list }) {
+      return {
+        title: removeMarkdown(heading),
+        subtitle: countItems(list.length),
+      };
+    }
+  }
+}
+const UnorderedList = {
+  name: 'UnorderedList',
+  title: 'Sekcja z listą nienumerowaną',
+  type: 'object',
+  fields: [
+    {
+      name: 'heading',
+      type: 'markdown',
+      title: 'Nagłówek',
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: 'paragraph',
+      type: 'markdown',
+      title: 'Paragraf (opcjonalnie)',
+    },
+    {
+      name: 'list',
+      type: 'array',
+      of: [
+        { type: 'markdown' }
+      ],
+      title: 'Lista',
+      validation: Rule => Rule.required(),
+    },
+  ],
+  preview: {
+    select: {
+      heading: 'heading',
+      list: 'list',
+    },
+    prepare({ heading, list }) {
+      return {
+        title: removeMarkdown(heading),
+        subtitle: countItems(list.length),
+      };
+    }
+  }
+}
+const Standout = {
+  name: 'Standout',
+  title: 'Sekcja wyróżniona',
+  type: 'object',
+  fields: [
+    {
+      name: 'heading',
+      type: 'markdown',
+      title: 'Nagłówek',
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: 'paragraph',
+      type: 'markdown',
+      title: 'Paragraf',
+      validation: Rule => Rule.required(),
+    },
+  ],
+  preview: {
+    select: {
+      heading: 'heading',
+      paragraph: 'paragraph',
+    },
+    prepare({ heading, paragraph }) {
+      return {
+        title: removeMarkdown(heading),
+        subtitle: removeMarkdown(paragraph),
+      };
+    }
+  }
+}
+const TextSection = {
+  name: 'TextSection',
+  title: 'Sekcja tekstowa',
+  type: 'object',
+  fields: [
+    {
+      name: 'heading',
+      type: 'markdown',
+      title: 'Nagłówek',
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: 'paragraph',
+      type: 'markdown',
+      title: 'Paragraf',
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: 'secondParagraph',
+      type: 'markdown',
+      title: 'Drugi paragraf (opcjonalnie)',
+    },
+  ],
+  preview: {
+    select: {
+      heading: 'heading',
+      paragraph: 'paragraph',
+    },
+    prepare({ heading, paragraph }) {
+      return {
+        title: removeMarkdown(heading),
+        subtitle: removeMarkdown(paragraph),
+      };
+    }
+  }
+}
+
 export default {
   name: 'product',
   title: 'Produkty',
@@ -145,7 +347,19 @@ export default {
         }),
       hidden: ({ document }) => document.type !== 'bundle',
     },
-    // TODO: Add product description
+    // TODO: Add product
+    {
+      name: 'description',
+      type: 'array',
+      title: 'Opis',
+      of: [
+        ColumnImageSection,
+        OrderedList,
+        Standout,
+        UnorderedList,
+        TextSection,
+      ],
+    },
     {
       name: 'parameters',
       type: 'array',
