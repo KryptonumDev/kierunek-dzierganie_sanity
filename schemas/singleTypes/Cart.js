377c22cb45093eec646490902e24d870b6bfd362
kryptonumstudio <kryptonumstudio@gmail.com>
2024-04-16 18:37:21 +0200

    fix related products in cart

--- schemas/singleTypes/Cart.js
+++ schemas/singleTypes/Cart.js
@@ -8,7 +8,7 @@ export default {
       name: 'highlighted',
       type: 'array',
       title: 'Wyróżnione produkty',
-      of: [{ type: 'reference', to: [{ type: 'product' }] }],
+      of: [{ type: 'reference', to: [{ type: 'product' }, { type: 'course' }, { type: 'bundle' }] }],
       Rule: Rule => Rule.max(2).required(),
     },
   ],
